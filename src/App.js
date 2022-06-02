e3e65579b3684b369e1184c1c8037e23410b9273
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-06-02 20:52:59 +0200

    chore: add useProviderLink

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
-import Layout from './components/Layout';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import Dashboard from './pages/Dashboard';
-import Profile from './pages/Profile';
+import Layout from './components/Layout'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Profile from './pages/Profile'
+import { NhostClient, NhostReactProvider } from '@nhost/react'
+
+const nhost = new NhostClient({
+  backendUrl: process.env.REACT_APP_NHOST_BACKEND_URL || '',
+})
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="sign-up" element={<SignUp />} />
-          <Route path="sign-in" element={<SignIn />} />
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <NhostReactProvider nhost={nhost}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='sign-up' element={<SignUp />} />
+            <Route path='sign-in' element={<SignIn />} />
+            <Route path='/' element={<Layout />}>
+              <Route index element={<Dashboard />} />
+              <Route path='profile' element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
 
-      <Toaster />
+        <Toaster />
+      </NhostReactProvider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,16 @@
-import styles from '../styles/pages/Dashboard.module.css';
+import styles from '../styles/pages/Dashboard.module.css'
 
-import { useOutletContext } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import { useOutletContext } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
+import { useProviderLink } from '@nhost/react'
+import { useEffect } from 'react'
 
 const Dashboard = () => {
-  const { user } = useOutletContext();
-
+  const { user } = useOutletContext()
+  const { google } = useProviderLink({ redirectTo: '/dashboard' })
+  useEffect(() => {
+    console.log('goo', google)
+  }, [google])
   return (
     <>
       <Helmet>
@@ -17,10 +22,11 @@ const Dashboard = () => {
 
         <p className={styles['welcome-text']}>
           Welcome, {user?.metadata?.firstName || 'stranger'}{' '}
-          <span role="img" alt="hello">
+          <span role='img' alt='hello'>
             👋
           </span>
         </p>
+        <a href={google}>Authenticate with Google</a>
 
         <p className={styles['info-text']}>
           Edit the <code>src/pages/Dashboard.js</code> file to populate this
@@ -28,7 +34,7 @@ const Dashboard = () => {
         </p>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
